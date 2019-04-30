f2761e6626d9a5e0febb385fbc72760f88b5f2ac
Mat Bettinson <mat@plothatching.com>
2019-04-30 09:41:05 +0930

    replace external random color package

--- src/particles.ts
+++ src/particles.ts
@@ -1,5 +1,3 @@
-import { randomColor } from 'randomcolor'
-
 export class Particle {
   alive: boolean = true
   radius: number
@@ -78,7 +76,7 @@ export class Particles {
     this.canvas = canvas
     this.ctx = this.canvas.getContext('2d')
     this.ctx.globalCompositeOperation  = 'color'
-    this.colors = randomColor({count: 20})
+    this.colors = this.getRandomColors(20)
   }
   
   begin(recordMode: boolean = false) {
@@ -219,6 +217,35 @@ export class Particles {
   }
   clearLastParp() {
     this.lastParp = null
+  }
+
+  getRandomColors(num: number): string[] {
+    let colors: string[] = []
+    for (let i = 0; i < num; ++i) {
+      colors.push(this.rainbow(num, i))
+    }
+    return colors
+  }
+
+  rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch(i % 6){
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
+    }
+    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    return (c);
   }
 
 }
